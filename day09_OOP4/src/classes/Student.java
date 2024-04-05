3488c56b9dca66fb2b0d8157f6e57f99a180a1a1
SionNoh <vmfpsel@gmail.com>
2024-04-05 12:34:19 +0900

    day 9 setup

---
+++ day09_OOP4/src/classes/Student.java
@@ -0,0 +1,65 @@
+package classes;
+
+public class Student {
+
+	private String id;
+	private String name;
+	private int korean;
+	private int english;
+	private int math;
+	
+	// 국어, 영어, 수학의 총점을 구하는 메서드
+	public int getSum() {
+		return korean + english + math;
+	}
+	
+	// 점수의 평균을 구하는 메서드
+	public float getAvg() {
+		return (float)getSum()/3;
+	}
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getKorean() {
+		return korean;
+	}
+	public void setKorean(int korean) {
+		this.korean = korean;
+	}
+	public int getEnglish() {
+		return english;
+	}
+	public void setEnglish(int english) {
+		this.english = english;
+	}
+	public int getMath() {
+		return math;
+	}
+	public void setMath(int math) {
+		this.math = math;
+	}
+	
+	public Student(String id, String name, int korean, int english, int math) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.korean = korean;
+		this.english = english;
+		this.math = math;
+	}
+	
+	public Student() {
+		super();
+	}
+
+}
