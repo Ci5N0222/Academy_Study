c13730e1ac4befb82e973baad0a7e941a00f496e
SionNoh <vmfpsel@gmail.com>
2024-04-08 12:51:09 +0900

    문제발생: 상속, 다형성, 컬렉션을 사용하여 문제 해결 대기

--- Day_10_OOP2/src/dao/MemberManager.java
+++ Day_10_OOP2/src/dao/MemberManager.java
@@ -1,38 +1,66 @@
 package dao;
 
+import classes.Gold;
 import classes.Silver;
 
 public class MemberManager {
 	
 	Silver[] silver = new Silver[10];
-	int index = 0;
+	Gold[] gold = new Gold[10];
 	
-	// index를 넘겨주는 메서드
+	int silverIndex = 0;
+	int goldIndex = 0;
+	
+	// Silver index를 넘겨주는 메서드
 	public int getIndex() {
-		return index;
+		return silverIndex;
+	}
+	
+	// Gold index를 넘겨주는 메서드
+	public int getGoldIndex() {
+		return goldIndex;
 	}
 	
 	// Member를 추가하는 메서드
 	public int addMember(String id, String name, int point) {
-		if(index == 10) {
+		if(silverIndex == 10) {
 			return 0;
 		} else {
 			// 회원 추가 로직
 			Silver sv = new Silver(id, name, point);
-			silver[index++] = sv;
+			silver[silverIndex++] = sv;
 			return 1;
 		}
 	}
 	
-	// Member를 반환하는 메서드
+	public int addGoldMember(String id, String name, int point) {
+		if(goldIndex == 10) {
+			return 0;
+		} else {
+			// 회원 추가 로직
+			Gold gd = new Gold(id, name, point);
+			gold[goldIndex++] = gd;
+			return 1;
+		}
+	}
+	
+	
+	// Silver Member를 반환하는 메서드
 	public Silver[] getMembers() {
-		// index 길이 만큼만 리턴 --> 나중에 작업
-		// Silver[] sv = new Silver[index];
-		if(index == 0) {
+		if(silverIndex == 0) {
 			return null;
 		} else {
 			// 회원 정보 출력 로직
 			return silver;
+		}
+	}
+	// Gold Member를 반환하는 메서드	
+	public Gold[] getGoldMembers() {
+		if(goldIndex == 0) {
+			return null;
+		} else {
+			// 회원 정보 출력 로직
+			return gold;
 		}
 	}
 	
--- Day_10_OOP2/src/main/MainView.java
+++ Day_10_OOP2/src/main/MainView.java
@@ -2,6 +2,7 @@ package main;
 
 import java.util.Scanner;
 
+import classes.Gold;
 import classes.Silver;
 import dao.MemberManager;
 
@@ -11,11 +12,19 @@ public class MainView {
 		
 		/**
 		 *  회원 관리 시스템
-		 *  1. 신규회원 등록
-		 *  2. 회원 목록 출력
+		 *  	1. 신규회원 등록
+		 *  	2. 회원 목록 출력
 		 *  
 		 *  추가 사항
-		 *  1. 골드 등급 추가 요청
+		 *  	1. 골드 등급 추가 요청 --> Gold 클래스 추가 작업 진행
+		 *  
+		 *  추가 작업을 진행한 현 회원관리 시스템의 치명적인 문제점
+		 *  	1. 코드 중복: 같은 코드로 작성된 클래스가 중복됨
+		 *  	  --> 상속을 이용하여 해결
+		 *		2. 코드 결합도: 유지보수 및 처리에 불편함 (MemberManager의 클래스 간 의존도가 높다 (결합도가 높다))
+		 *		  --> 다형성을 이용하여 해결
+		 * 		3. 저장소 문제: 배열이라는 것 자체가 저장소로 부적합하다.
+		 * 		  --> 컬렉션을 이용하여 해결
 		 */
 		
 		Scanner sc = new Scanner(System.in);
@@ -30,22 +39,25 @@ public class MainView {
 			int input = Integer.parseInt(sc.nextLine());
 			
 			if(input == 1) {
-				// Create new members
-				int point = 1000;
-				System.out.println("아이디를 입력해주세요.");
-				String id = sc.nextLine();
-				System.out.println("이름을 입력해주세요.");
-				String name = sc.nextLine();
-				int success = manager.addMember(id, name, point);
-				
-				// Create success or fail
-				if(success == 1) System.out.println("!!! 회원 등록 성공 !!!");
-				else System.out.println("더 이상 회원을 추가할 수 없습니다.");
+//				// Create new members
+//				int point = 1000;
+//				System.out.println("아이디를 입력해주세요.");
+//				String id = sc.nextLine();
+//				System.out.println("이름을 입력해주세요.");
+//				String name = sc.nextLine();
+//				int success = manager.addMember(id, name, point);
+//				
+//				// Create success or fail
+//				if(success == 1) System.out.println("!!! 회원 등록 성공 !!!");
+//				else System.out.println("더 이상 회원을 추가할 수 없습니다.");
+				manager.addMember("sv1", "kim", 1000);
+				manager.addMember("sv2", "lee", 2000);
+				manager.addGoldMember("gd1", "park", 5000);
 				
 			} else if(input == 2) {
 				// Members list
 				Silver[] silver = manager.getMembers();
-				if(silver == null) System.out.println("등록된 회원이 없습니다");
+				if(silver == null) System.out.println("등록된 실버 회원이 없습니다");
 				else {
 					System.out.println("ID\t이름\t포인트\t보너스");
 					for(int i=0; i<manager.getIndex(); i++) {
@@ -53,6 +65,18 @@ public class MainView {
 										   silver[i].getName() + "\t" +
 										   silver[i].getPoint() + "\t" +
 										   silver[i].getBonus() + "\t");
+					}
+				}
+				
+				Gold[] gold = manager.getGoldMembers();
+				if(gold == null) System.out.println("등록된 실버 회원이 없습니다");
+				else {
+					if(silver == null)System.out.println("ID\t이름\t포인트\t보너스");
+					for(int i=0; i<manager.getGoldIndex(); i++) {
+						System.out.println(gold[i].getId() + "\t" +
+										   gold[i].getName() + "\t" +
+									  	   gold[i].getPoint() + "\t" +
+										   gold[i].getBonus() + "\t");
 					}
 				}
 				
