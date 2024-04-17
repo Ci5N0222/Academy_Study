a902a0baf11eb14df15426cb32801ac7fcbeb941
Sion-Noh <vmfpsel@gmail.com>
2024-04-17 12:25:14 +0900

    setup

---
+++ Day13_Thread/src/exam/Exam01.java
@@ -0,0 +1,17 @@
+package exam;
+
+public class Exam01 {
+
+	public static void main(String[] args) {
+		
+		for(int i=0; i<100; i++) {
+			System.out.println(i);
+		}
+		
+		for(int i=0; i<100; i++) {
+			System.out.println(i);
+		}
+		
+	}
+
+}
