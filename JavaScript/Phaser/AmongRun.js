a254e8c0daf50fe83fac035e058982dbad2f47d7
C-ON <vmfpsel@gmail.com>
2024-06-17 09:30:36 +0900

    game update

--- JavaScript/Phaser/AmongRun.js
+++ JavaScript/Phaser/AmongRun.js
@@ -4,9 +4,13 @@ class AmongRun extends Phaser.Scene {
         this.frame = 0;
         this.timer = 0;
         this.score = 0;
-        this.tileSpeed = 2;
         this.jumpCount = 0;
         this.items = [];
+        
+        this.tileSpeed = 2;
+        this.charactorSpeed = 250;
+        this.itemSpeed = 150;
+        this.motionSpeed = 11;
     }
 
     preload(){
@@ -36,37 +40,38 @@ class AmongRun extends Phaser.Scene {
         this.player.setScale(playerScaleFactor);
         this.player.setSize(50/playerScaleFactor, 80/playerScaleFactor);
 
-        // player charactor attribute
-        this.player.setGravity(0, 1000);            // gravity
-        this.player.setCollideWorldBounds(true);    // below the range
-        this.player.anims.play({key:"run"});
-        this.player.setData("onfloor", true);
-
         // player charactor "run" motion
         this.anims.create({
-           key: "run",
-           frames: this.anims.generateFrameNumbers("amongRun", {start:0, end:7}),
-           frameRate: 11,
-           repeat: -1
+            key: "run",
+            frames: this.anims.generateFrameNumbers("amongRun", {start:0, end:7}),
+            frameRate: this.motionSpeed,
+            repeat: -1
         });
 
         // player charactor "jump" motion
         this.anims.create({
-           key: "jump",
-           frames: this.anims.generateFrameNumbers("amongJump", {start:0, end:9}),
-           frameRate: 12,
-           repeat: 1
+            key:"jump",
+            frames: this.anims.generateFrameNumbers("amongJump", {start:0, end:9}),
+            frameRate: this.motionSpeed,
+            repeat: 1
         });
+
+        // player charactor attribute
+        this.player.setCollideWorldBounds(true);    // below the range
+        this.player.setGravity(0, 1000);            // gravity
+        this.player.anims.play({key:"run"});
+        this.player.setData("onFloor", true);
 
         // floor
         let floor = this.add.rectangle(0, this.cameras.main.height-30, this.cameras.main.width, 5, 0x0000, 0);
-        this.physics.add.existing(floor, true);
         floor.setOrigin(0, 0);
-
+        this.physics.add.existing(floor, true);
+        
         // final boundary ( item instance delete )
         let finalBoundary = this.add.rectangle(this.cameras.main.height-20, 0, 5, this.cameras.main.height, 0x0000);
-        this.physics.add.existing(finalBoundary, true);
         finalBoundary.setOrigin(100, 0);
+        this.physics.add.existing(finalBoundary, true);
+        
         
 
         /** ============================ A and B Event ============================ **/
@@ -103,6 +108,12 @@ class AmongRun extends Phaser.Scene {
             document.querySelector("#timer").innerHTML = this.timer;
         }
 
+        // 60 second Game over
+        if(this.timer === 60) {
+            this.scene.start("GameOver");
+        }
+
+        // item regen
         if(this.frame%40 == 0) {
             // item 
             let item = this.physics.add.sprite(720, Math.random()*(360-110+1)+110, "item");
@@ -112,20 +123,22 @@ class AmongRun extends Phaser.Scene {
             item.setScale(itemScaleFactor);
             item.setSize(40/itemScaleFactor, 50/itemScaleFactor);
             
-            item.setVelocityX(-150);
+            item.setVelocityX(this.itemSpeed*-1);
             this.items.push(item);
         }
 
         this.background.tilePositionX += this.tileSpeed;
 
+        // Player Charactor Speed
         if(this.cursor.left.isDown) {
-            this.player.setVelocityX(-250);
+            this.player.setVelocityX(this.charactorSpeed*-1);
         } else if(this.cursor.right.isDown) {
-            this.player.setVelocityX(250);
+            this.player.setVelocityX(this.charactorSpeed);
         } else {
             this.player.setVelocityX(0);
         }
 
+        // Player Charactor 2 Jump
         if(Phaser.Input.Keyboard.JustDown(this.cursor.space)){
             if(this.jumpCount < 2){
                 this.player.setVelocityY(-500);
@@ -135,6 +148,7 @@ class AmongRun extends Phaser.Scene {
             }
         }
 
+        // Player Charactor motion change
         if(this.player.getData("onFloor") && this.player.anims.currentAnim.key !== "run"){
             this.player.play("run");
             this.jumpCount = 0;
