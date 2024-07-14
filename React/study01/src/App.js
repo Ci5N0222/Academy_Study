b3c8c59fc9ea0fd2fdf6adbdb779454922f9716b
Sion-Noh <vmfpsel@gmail.com>
2024-07-14 20:26:52 +0900

    components directory

--- React/study01/src/App.js
+++ React/study01/src/App.js
@@ -1,29 +1,12 @@
 import './App.css';
-
-const TableBox = () => {
-  return (
-    <table border="1" align="center">
-      <tr>
-        <th colspan="3">리액트 연습</th>
-      </tr>
-      <tr>
-        <th>seq</th>
-        <th>name</th>
-        <th>price</th>
-      </tr>
-      <tr>
-        <td>1</td>
-        <td>apple</td>
-        <td>1000</td>
-      </tr>
-    </table>
-  );
-}
+import { TableBox } from './components/TableBox/TableBox';
+import { UlBox } from './components/UlBox/UlBox';
 
 const App = () => {
   return (
     <div>
       <TableBox />
+      <UlBox />
     </div>
   );
 }
---
+++ React/study01/src/components/TableBox/TableBox.jsx
@@ -0,0 +1,19 @@
+export const TableBox = () => {
+  return (
+    <table border="1">
+      <tr>
+        <th colspan="3">리액트 연습</th>
+      </tr>
+      <tr>
+        <th>seq</th>
+        <th>name</th>
+        <th>price</th>
+      </tr>
+      <tr>
+        <td>1</td>
+        <td>apple</td>
+        <td>1000</td>
+      </tr>
+    </table>
+  );
+}
