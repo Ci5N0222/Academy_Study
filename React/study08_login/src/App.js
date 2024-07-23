fda84cc6ae61ada3586d4501f8021f8bddd60747
C-ON <vmfpsel@gmail.com>
2024-07-23 12:44:19 +0900

    study08 login study templet setting

---
+++ React/study08_login/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <table>
+        <tr>
+        </tr>
+      </table>
+    </div>
+  );
+}
+
+export default App;
