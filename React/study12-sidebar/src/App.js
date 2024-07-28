793f2cb06a1056f9b38d8024196e469961d172a7
Sion-Noh <vmfpsel@gmail.com>
2024-07-28 22:13:01 +0900

    반응형 사이드바 study

--- React/study12-sidebar/src/App.js
+++ React/study12-sidebar/src/App.js
@@ -6,44 +6,17 @@ import {RiSettings4Line} from "react-icons/ri";
 import {AiOutlineHeart, AiOutlineUser} from "react-icons/ai";
 import {FiFolder, FiMessageSquare, FiShoppingCart} from "react-icons/fi";
 import {TbReportAnalytics} from "react-icons/tb";
+import {Side} from "./components/Side/Side";
 
 function App() {
 
-  const menus = [
-    {name: "dashboard", link: "/", icon: MdOutlineDashboard},
-    {name: "User", link: "/", icon: AiOutlineUser},
-    {name: "Messages", link: "/", icon: FiMessageSquare},
-    {name: "Analytics", link: "/", icon: TbReportAnalytics, margin: true},
-    {name: "File Manager", link: "/", icon: FiFolder},
-    {name: "Cart", link: "/", icon: FiShoppingCart},
-    {name: "Saved", link: "/", icon: AiOutlineHeart, margin: true},
-    {name: "Setting", link: "/", icon: RiSettings4Line},
-  ]
 
   const [open, setOpen] = useState(true);
 
   return (
     <div className="container">
-      <div className={ open ? "side-full" : "side-short"}>
-        <div className="toggle">
-          <HiMenuAlt3 size={30} className="icons" onClick={() => setOpen(!open)} />
-        </div>
-        <div className="menus">
-          {
-            menus.map((menu, i) => {
-              return (
-                <a href={menu?.link} key={i} className="menu-link">
-                  <div>
-                    {React.createElement(menu?.icon, {size: "30"})}
-                  </div>
-                  <h3 className={open ? "menu-title" : "menu-title-action"}>{menu?.name}</h3>
-                </a>
-              );
-            })
-          }
-        </div>
-      </div>
-      <div className="heder">
+      <Side open={open} setOpen={setOpen} />
+      <div className="header">
         React Side Bar
       </div>
     </div>
---
+++ React/study12-sidebar/src/components/Side/Side.jsx
@@ -0,0 +1,46 @@
+import styles from './Side.module.css'
+import {HiMenuAlt3} from "react-icons/hi";
+import React from "react";
+import {MdOutlineDashboard} from "react-icons/md";
+import {AiOutlineHeart, AiOutlineUser} from "react-icons/ai";
+import {FiFolder, FiMessageSquare, FiShoppingCart} from "react-icons/fi";
+import {TbReportAnalytics} from "react-icons/tb";
+import {RiSettings4Line} from "react-icons/ri";
+
+export const Side = ({open, setOpen}) => {
+
+    const menus = [
+        {name: "dashboard", link: "/", icon: MdOutlineDashboard},
+        {name: "User", link: "/", icon: AiOutlineUser},
+        {name: "Messages", link: "/", icon: FiMessageSquare},
+        {name: "Analytics", link: "/", icon: TbReportAnalytics, margin: true},
+        {name: "File Manager", link: "/", icon: FiFolder},
+        {name: "Cart", link: "/", icon: FiShoppingCart},
+        {name: "Saved", link: "/", icon: AiOutlineHeart, margin: true},
+        {name: "Setting", link: "/", icon: RiSettings4Line},
+    ]
+
+    return (
+        <div className={styles.container}>
+            <div className={open ? styles.sideFull : styles.sideShort}>
+                <div className={styles.toggle}>
+                    <HiMenuAlt3 size={30} className="icons" onClick={() => setOpen(!open)}/>
+                </div>
+                <div className={styles.menus}>
+                    {
+                        menus.map((menu, i) => {
+                            return (
+                                <a href={menu?.link} key={i} className={styles.menuLink}>
+                                    <div>
+                                        {React.createElement(menu?.icon, {size: "30"})}
+                                    </div>
+                                    <h3 className={open ? styles.menuTitle : styles.menuTitleAction }>{menu?.name}</h3>
+                                </a>
+                            );
+                        })
+                    }
+                </div>
+            </div>
+        </div>
+    );
+}
