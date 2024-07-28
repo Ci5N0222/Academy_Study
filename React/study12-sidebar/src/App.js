25bc0d19f6fe7d238eae83c57329fc6dc787d1c5
C-ON <vmfpsel@gmail.com>
2024-07-28 19:44:14 +0900

    react sidebar study

--- React/study12-sidebar/src/App.js
+++ React/study12-sidebar/src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from "react"
+import { HiMenuAlt3 } from 'react-icons/hi';
+import { MdOutlineDashboard } from "react-icons/md";
+import {RiSettings4Line} from "react-icons/ri";
+import {AiOutlineHeart, AiOutlineUser} from "react-icons/ai";
+import {FiFolder, FiMessageSquare, FiShoppingCart} from "react-icons/fi";
+import {TbReportAnalytics} from "react-icons/tb";
 
 function App() {
+
+  const menus = [
+    {name: "dashboard", link: "/", icon: MdOutlineDashboard},
+    {name: "User", link: "/", icon: AiOutlineUser},
+    {name: "Messages", link: "/", icon: FiMessageSquare},
+    {name: "Analytics", link: "/", icon: TbReportAnalytics, margin: true},
+    {name: "File Manager", link: "/", icon: FiFolder},
+    {name: "Cart", link: "/", icon: FiShoppingCart},
+    {name: "Saved", link: "/", icon: AiOutlineHeart, margin: true},
+    {name: "Setting", link: "/", icon: RiSettings4Line},
+  ]
+
+  const [open, setOpen] = useState(true);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className={ open ? "side-full" : "side-short"}>
+        <div className="toggle">
+          <HiMenuAlt3 size={30} className="icons" onClick={() => setOpen(!open)} />
+        </div>
+        <div className="menus">
+          {
+            menus.map((menu, i) => {
+              return (
+                <a href={menu?.link} key={i} className="menu-link">
+                  <div>
+                    {React.createElement(menu?.icon, {size: "30"})}
+                  </div>
+                  <h3 className={open ? "menu-title" : "menu-title-action"}>{menu?.name}</h3>
+                </a>
+              );
+            })
+          }
+        </div>
+      </div>
+      <div className="heder">
+        React Side Bar
+      </div>
     </div>
   );
 }
