66fe88b9d769f42a95b690977ae05c73c163d09f
Sion-Noh <vmfpsel@gmail.com>
2024-08-10 19:50:49 +0900

    study

--- React/study06_justand/src/store/store.js
+++ React/study06_justand/src/store/store.js
@@ -34,7 +34,7 @@ export const useBoardStore = create(set => ({
   boards: [
     { id: 1, title: "리액트 어렵다", content: "리액트 초 어렵다",  writer: "스프링장인"},
     { id: 2, title: "스프링 어렵다", content: "스프링 초 어렵다",  writer: "리액트장인"},
-    { id: 3, title: "리액트 쉽다", content: "리액트 초 쉽다",  writer: "리액트장인"},
+    { id: 3, title: "리액트 쉽다", content: "리액트 초 쉽다",  writer: "리액트장인"}, 
     { id: 4, title: "스프링 쉽다", content: "스프링 초 쉽다",  writer: "스프링장인"},
   ],
   addBoard: param => set(prev => ({ boards: [ ...prev.boards, param ] })),
