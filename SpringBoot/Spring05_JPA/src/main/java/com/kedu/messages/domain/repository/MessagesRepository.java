3d224cdbe529e5adcddad60154f76cacd26eef79
Sion-Noh <vmfpsel@gmail.com>
2024-09-02 19:41:21 +0900

    JPA study

--- SpringBoot/Spring05_JPA/src/main/java/com/kedu/messages/domain/repository/MessagesRepository.java
+++ SpringBoot/Spring05_JPA/src/main/java/com/kedu/messages/domain/repository/MessagesRepository.java
@@ -3,6 +3,7 @@ package com.kedu.messages.domain.repository;
 
 import com.kedu.messages.domain.entity.Messages;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -15,12 +16,19 @@ public interface MessagesRepository extends JpaRepository<Messages, Long> {
 
     List<Messages> findByWriter(String writer);
 
-    /**
+    /*
      *  JpaRepository는 메서드의 이름이 쿼리를 만들어 낸다. (임의로 짓는 메서드 이름을 지어선 안됨)
      *  findBy로 시작하여야 한다.
      *  뒤쪽엔 컬럼명이 들어와야 한다.
      *
-     *  컬러명 / and, or / GreaterThan / LessThan / Containing
+     *  컬럼명 / and, or / GreaterThan / LessThan / Containing
      */
+
+
+    // 1. Repository 메서드 네이밍 쿼리
+    // 2. JPQL : JPA 메서드 이름으로 생성하는 쿼리에 한계가 있을 경우 직접 쿼리를 적용하는 방법
+    // 3. Native SQL
+    @Query(value = "select * from messages order by desc", nativeQuery = true)
+    List<Messages> abc();
 
 }
